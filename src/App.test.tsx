95b8e75b6513066b3d79fffe7f0c6ac0c3fa6504
jkxue <jkxupt@163.com>
2020-03-29 20:00:54 +0800

    fix: fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should display name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = getByText(/JK Xue/i);
+  expect(element).toBeInTheDocument();
 });
